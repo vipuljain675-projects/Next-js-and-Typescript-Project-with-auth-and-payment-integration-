3637901b5452c1268115a36e60449971137d8259
Vipul Jain <vipuljain675@gmail.com>
2026-01-23 12:53:29 +0530

    Done eith experiences and services

---
+++ frontend/app/experiences/page.tsx
@@ -0,0 +1,3 @@
+export default function Experiences() {
+  return <div className="p-5 mt-5"><h1>Experiences Coming Soon!</h1></div>;
+}
---
+++ frontend/app/services/page.tsx
@@ -0,0 +1,3 @@
+export default function Services() {
+  return <div className="p-5 mt-5"><h1>Services Coming Soon!</h1></div>;
+}
